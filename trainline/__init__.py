4c0f312e8d2ec675385e3fefa6ff5998a8e587cd
Andrew Fray <andrew.fray@gmail.com>
2022-12-22 22:36:26 +0000

    better zeep setup
    and starting to figure out more train details

--- trainline/__init__.py
+++ trainline/__init__.py
@@ -3,7 +3,6 @@ import zeep
 import azure.functions as func
 import os
 
-
 def main(req: func.HttpRequest) -> func.HttpResponse:
     left_crs = req.params.get('left_crs')
     right_crs = req.params.get('right_crs')
@@ -22,15 +21,16 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
         return func.HttpResponse("expected valid parameters", status_code=400)
     
     logging.info(f'looking for trains between {left_crs} and {right_crs}')
-        
-    ldbws = zeep.Client("https://lite.realtime.nationalrail.co.uk/OpenLDBWS/wsdl.aspx?ver=2021-11-01")
-    headers = {
-        "AccessToken": os.environ["ldbwsAuth"],
-    }
-    with ldbws.settings(strict=False):
-        bhm_to_knn = ldbws.service.GetArrivalBoard(crs=right_crs, filterType="from", filterCrs=left_crs, numRows=2, _soapheaders=headers)
-        
-        for service in bhm_to_knn.trainServices.service:
-            logging.info(service.sta)
-        return func.HttpResponse("placeholder")
+    
+    loose_settings = zeep.Settings(strict=False)
+    ldbws = zeep.Client("https://lite.realtime.nationalrail.co.uk/OpenLDBWS/wsdl.aspx?ver=2021-11-01", settings=loose_settings)
+    ldbws.set_default_soapheaders({"AccessToken": os.environ["ldbwsAuth"]})
+
+    left_to_right = ldbws.service.GetArrivalBoard(crs=right_crs, filterType="from", filterCrs=left_crs, numRows=2)
+    
+    lr_train_details = [ldbws.service.GetServiceDetails(serviceID=service.serviceID)
+                        for service in left_to_right.trainServices.service]
+    print(lr_train_details)
+
+    return func.HttpResponse("placeholder")
 
