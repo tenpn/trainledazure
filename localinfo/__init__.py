354a4f58fbcf853910a7a033181a76d635ec3599
Andrew Fray <andrew.fray@gmail.com>
2023-01-06 21:17:47 +0000

    started putting in train times
    but there's strikes so here's fake info!!

--- localinfo/__init__.py
+++ localinfo/__init__.py
@@ -1,5 +1,6 @@
 import logging
 import azure.functions as func
+import zeep
 import json
 import os
 import requests
@@ -60,6 +61,41 @@ def put_weather_into(postcode : str, response: Dict) -> Dict:
     
     return response
 
+def get_details_from_service(train_service, ldbws):
+    """makes sure the service is valid
+
+    Args:
+        train_service (ServiceItem): 
+        ldbws: it's ldbws!!!
+
+    Returns:
+        ServiceDetails: makes sure sta is vaid and populated
+    """
+    details = ldbws.service.GetServiceDetails(train_service.serviceID)
+    if details.std is None:
+        details.std = train_service.std
+    return details
+
+def put_trains_into(station: str, response: Dict) -> Dict:
+    loose_settings = zeep.Settings(strict=False)
+    ldbws = zeep.Client("https://lite.realtime.nationalrail.co.uk/OpenLDBWS/wsdl.aspx?ver=2021-11-01", settings=loose_settings)
+    ldbws.set_default_soapheaders({"AccessToken": os.environ["ldbwsAuth"]})
+    departures = ldbws.service.GetDepartureBoard(crs=station, numRows=10)
+    
+    response["departures_times"] = [21.40, 21.55, 22.10]
+    
+    services = departures.trainServices
+    if services is None:
+        return response
+
+    service_list = services.service
+    if service_list is None:
+        return response
+
+    response["departures_times"] = [21.40, 21.55, 22.10]
+    return response
+
+
 def main(req: func.HttpRequest) -> func.HttpResponse:
     train_crs = req.params.get('train')
     weather_postcode = req.params.get('weather')
@@ -78,6 +114,7 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
         return func.HttpResponse("expected valid parameters", status_code=400)
     
     response = put_weather_into(weather_postcode, {})
+    response = put_trains_into(train_crs, response)
 
     logging.info(str(response))
     return func.HttpResponse(json.dumps(response))
